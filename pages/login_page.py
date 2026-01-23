a21b5b909ca0ab4ae918226ce15aa800daad4bee
we1n13 <fedotov.dm13@gmail.com>
2026-01-24 02:48:43 +0300

    first POM page

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,22 +3,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
+from base.base_class import Base
+
 base_url = 'https://www.saucedemo.com'
 login_standart = 'standard_user'
 password_all = 'secret_sauce'
 
-class Login_page:
+class Login_page(Base):
+    base_url = 'https://www.saucedemo.com'
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
-    def authorization(self, login_name, login_password):
+    #Locators
 
-        user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(('id', 'user-name')))
-        user_name.send_keys(login_name)
-        print("Логин введен")
+    user_name = '//input[@id="user-name"]'
+    password = '//input[@id="password"]'
+    login_btn = '//input[@id="login-button"]'
 
-        password = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(('id', 'password')))
-        password.send_keys(login_password)
-        password.send_keys(Keys.ENTER)
-        print("Вход выполнен")
+    #Getters
+
+    def get_user_name(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(('xpath', self.user_name)))
+
+    def get_password(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(('xpath', self.password)))
+
+    def get_login_btn(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(('xpath', self.login_btn)))
+
+    #Actions
+
+    def input_user_name(self, user_name):
+        self.get_user_name().send_keys(user_name)
+        print("Input user name successfully")
+
+    def input_password(self, password):
+        self.get_password().send_keys(password)
+        print("Input password successfully")
+
+    def click_login_btn(self):
+        self.get_login_btn().click()
+        print("Click login button successfully")
+
+
+    #Methods
+
+    def authorization(self):
+        self.driver.get(self.base_url)
+        self.driver.maximize_window()
+        self.input_user_name("standard_user")
+        self.input_password("secret_sauce")
+        self.click_login_btn()
+
 
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,5 +1,5 @@
 import time
-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,9 +8,12 @@ from pages.login_page import Login_page
 
 users_list = ["standart_user", "locked_out_user", "problem_user", "performance_glitch_user"]
 
-def test_login(self):
+def test_login():
     driver = webdriver.Chrome()
     print("Starting test")
+
+    login = Login_page(driver)
+    login.authorization()
 
     enter_shopping_cart = WebDriverWait(driver, 30).until(
         EC.element_to_be_clickable(('xpath', '//div[id="shopping_cart_container"]')))
